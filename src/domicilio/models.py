5eb1637955217f4e171d7152bcf32f53e7481c38
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-06-08 21:44:55 -0500

    Creacion de la tabla en models.py de domicilio

--- src/domicilio/models.py
+++ src/domicilio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Domicilio(models.Model):
+    ciudad = models.TextField()
+    provincia = models.TextField()
+    departamento = models.TextField()

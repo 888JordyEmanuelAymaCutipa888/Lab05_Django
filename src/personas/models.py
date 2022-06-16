a2ab1833b072e973943ec815b30abbfcad2863cf
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-06-16 15:39:17 -0500

    Se inserta un cambio en el modelo, donde este acepta que el campo quede sin valor

--- src/personas/models.py
+++ src/personas/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Persona(models.Model):
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
-    edad = models.IntegerField(max_digits=3)
+    edad = models.IntegerField()#(max_digits=3)
+    donador = models.BooleanField(null = True)
